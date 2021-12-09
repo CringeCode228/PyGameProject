b9e809b3f746b21f73f16d2bc71eb655e3d28e0a
Artemuy <bibikonfikus1234@gmail.com>
2021-12-09 18:00:27 +0300

    start

--- PhysicsEngine.py
+++ PhysicsEngine.py
@@ -1,27 +1,60 @@
+from GameEngine import *
 from math import pi
 
 
-class RigidBody:
-    pass
+class OnCollision(Event):
+    
+    def __init__(self):
+        super(OnCollision, self).__init__()
 
 
-class Collider:
+class RigidBody(Component):
 
-    def __init__(self, side, pos_x=0, pos_y=0, rot_x=0, rot_y=0):
-        self.side = side
-        self.position_x = pos_x
-        self.position_y = pos_y
-        self.rotation_x = rot_x
-        self.rotation_y = rot_y
+    def __init__(self, obj):
+        super(RigidBody, self).__init__(obj, "RigidBody")
+        self.velocity = Velocity()
+
+    def tick(self, fps):
+        self.obj.transform.position.x += self.velocity.line_velocity.x / fps
+        self.obj.transform.position.y += self.velocity.line_velocity.y / fps
+        self.obj.transform.rotation.x += self.velocity.angular_velocity.x / fps
+        self.obj.transform.rotation.y += self.velocity.angular_velocity.y / fps
+
+
+class Velocity:
+
+    def __init__(self):
+        self.line_velocity = Vector2()
+        self.angular_velocity = Vector2()
+
+
+class Collider(Component):
+
+    def __init__(self, obj):
+        super(Collider, self).__init__(obj, "Collider")
+
+    def calculate(self, scene):
+        pass
 
 
 class CircleCollider(Collider):
 
-    def __init__(self):
-        super().__init__(pi)
+    def __init__(self, obj):
+        super().__init__(obj)
 
 
 class BoxCollider(Collider):
 
-    def __init__(self):
-        super().__init__(1)
+    def __init__(self, obj):
+        super().__init__(obj)
+
+    def calculate(self, scene):
+        for obj in scene.objects:
+            for component in obj.components:
+                if type(component) == BoxCollider:
+                    # TODO: calculate collision with box collider
+                    collision = False
+                    if collision:
+                        self.obj.on_collision()
+                        obj.on_collision()
+                    pass
