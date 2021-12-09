129f6192acb3d277191281f1a77b9aebdd562c0b
Artemuy <bibikonfikus1234@gmail.com>
2021-12-09 20:42:04 +0300

    Base Input-Output System

--- GameEngine.py
+++ GameEngine.py
@@ -19,6 +19,10 @@ class Object:
         self.z_sorting = z_sorting
         self.components = []
 
+    def tick(self):
+        for component in self.components:
+            component.tick()
+
     def render(self, screen):
         pass
 
@@ -31,22 +35,86 @@ class Object:
     def delete_component(self, component):
         self.components.remove(component)
 
+    def on_collision(self):
+        pass
+
 
 class Transform:
 
-    def __init__(self, pos_x=0, pos_y=0, rot_x=0, rot_y=0, scl_x=1, scl_y=1):
-        self.position_x = pos_x
-        self.position_y = pos_y
-        self.rotation_x = rot_x
-        self.rotation_y = rot_y
-        self.scale_x = scl_x
-        self.scale_y = scl_y
+    def __init__(self):
+        self.position = Vector2()
+        self.rotation = Vector2()
+        self.scale = Vector2()
 
 
 class Component:
 
-    def __init__(self):
+    def __init__(self, obj, name):
+        self.obj = obj
+        self.name = name
+
+    def tick(self, fps):
         pass
+
+
+class Event:
+
+    def __init__(self):
+        self.subs = []
+
+    def __call__(self, *args, **kwargs):
+        for sub in self.subs:
+            sub(*args, **kwargs)
+
+    def __iadd__(self, other):
+        self.subs.append(other)
+
+
+class Vector2:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+
+class Input:
+
+    codes_keys = {119: 'w', 97: 'a', 100: 'd', 115: 's', 113: 'q', 101: 'e', 114: 'r', 102: 'f', 122: 'z', 99: 'c',
+                  118: 'v', 120: 'x', 116: 't', 103: 'g', 98: 'b', 27: 'esc', 49: '1', 50: '2', 51: '3', 52: '4',
+                  53: '5', 54: '6', 55: '7', 56: '8', 57: '9', 48: '0'}
+
+    holding_keys = {"w": 0, "a": 0, "d": 0, "s": 0, "q": 0, "e": 0, "r": 0, "f": 0, "z": 0, "c": 0, "v": 0, "x": 0,
+                    "t": 0, "g": 0, "b": 0, "esc": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0,
+                    "8": 0,  "9": 0, "0": 0, "shift": 0, "tab": 0, "ctrl": 0}
+
+    pressed_keys = {"w": 0, "a": 0, "d": 0, "s": 0, "q": 0, "e": 0, "r": 0, "f": 0, "z": 0, "c": 0, "v": 0, "x": 0,
+                    "t": 0, "g": 0, "b": 0, "esc": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0,
+                    "8": 0,  "9": 0, "0": 0, "shift": 0, "tab": 0, "ctrl": 0}
+
+    released_keys = {"w": 0, "a": 0, "d": 0, "s": 0, "q": 0, "e": 0, "r": 0, "f": 0, "z": 0, "c": 0, "v": 0, "x": 0,
+                     "t": 0, "g": 0, "b": 0, "esc": 0, "1": 0, "2": 0, "3": 0, "4": 0, "5": 0, "6": 0, "7": 0,
+                     "8": 0,  "9": 0, "0": 0, "shift": 0, "tab": 0, "ctrl": 0}
+
+    @staticmethod
+    def get_key_down(key: str):
+        if Input.pressed_keys[key]:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_key(key: str):
+        if Input.holding_keys[key]:
+            return True
+        else:
+            return False
+
+    @staticmethod
+    def get_key_up(key: str):
+        if Input.released_keys[key]:
+            return True
+        else:
+            return False
 
 
 def clamp(value: Union[int, float], min_value: Union[int, float], max_value: Union[int, float]):
--- main.py
+++ main.py
@@ -1,3 +1,34 @@
 import pygame
 
+import GameEngine
 
+pygame.init()
+screen = pygame.display.set_mode((pygame.display.Info().current_w, pygame.display.Info().current_h), pygame.FULLSCREEN)
+pygame.display.set_caption("Example")
+
+pygame.display.flip()
+
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for key in GameEngine.Input.pressed_keys:
+        GameEngine.Input.pressed_keys[key] = 0
+    for key in GameEngine.Input.pressed_keys:
+        GameEngine.Input.pressed_keys[key] = 0
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key in GameEngine.Input.codes_keys:
+                GameEngine.Input.holding_keys[GameEngine.Input.codes_keys[event.key]] = 1
+            if event.key in GameEngine.Input.codes_keys:
+                GameEngine.Input.pressed_keys[GameEngine.Input.codes_keys[event.key]] = 1
+        elif event.type == pygame.KEYUP:
+            if event.key in GameEngine.Input.codes_keys:
+                GameEngine.Input.holding_keys[GameEngine.Input.codes_keys[event.key]] = 0
+            if event.key in GameEngine.Input.codes_keys:
+                GameEngine.Input.released_keys[GameEngine.Input.codes_keys[event.key]] = 1
+    clock.tick(60)
+pygame.quit()
