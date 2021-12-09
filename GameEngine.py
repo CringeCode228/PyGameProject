c95dbfdc670b975763d50270478c518fcd57bef3
Artemuy <bibikonfikus1234@gmail.com>
2021-12-09 15:49:51 +0300

    start

--- GameEngine.py
+++ GameEngine.py
@@ -1,10 +1,25 @@
 from typing import Union
 
 
+class Scene:
+
+    def __init__(self, *objects):
+        self.objects = []
+
+    def render(self):
+        for obj in sorted(self.objects, key=lambda key: obj.z_sorting):
+            obj.render()
+
+
 class Object:
 
-    def __init__(self):
+    def __init__(self, z_sorting = 0):
         self.transform = Transform()
+        self.parent = None
+        self.z_sorting = z_sorting
+
+    def render(self, screen):
+        pass
 
 
 class Transform:
@@ -13,8 +28,8 @@ class Transform:
         self.position_y = pos_x
         self.rotation_x = pos_x
         self.rotation_y = pos_x
-        self.scale_x = max(scl_x, 0.01)
-        self.scale_y = min(scl_y, 0.01)
+        self.scale_x = scl_x
+        self.scale_y = scl_y
 
 
 def clamp(value: Union[int, float], min_value: Union[int, float], max_value: Union[int, float]):
