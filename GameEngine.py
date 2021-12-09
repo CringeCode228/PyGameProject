214d2719e5cb00d2788e0a3cfca32ed463051f4b
Artemuy <bibikonfikus1234@gmail.com>
2021-12-09 15:57:20 +0300

    start

--- GameEngine.py
+++ GameEngine.py
@@ -13,23 +13,40 @@ class Scene:
 
 class Object:
 
-    def __init__(self, z_sorting = 0):
+    def __init__(self, z_sorting=0):
         self.transform = Transform()
         self.parent = None
         self.z_sorting = z_sorting
+        self.components = []
 
     def render(self, screen):
         pass
 
+    def add_component(self, component):
+        self.components.append(component)
+
+    def get_component(self, component):
+        return self.components[self.components.index(component)]
+
+    def delete_component(self, component):
+        self.components.remove(component)
+
 
 class Transform:
-    def __init__(self, pos_x=0, pos_y=0, rot_x=0, scl_x=1, scl_y=1):
+
+    def __init__(self, pos_x=0, pos_y=0, rot_x=0, rot_y=0, scl_x=1, scl_y=1):
         self.position_x = pos_x
-        self.position_y = pos_x
-        self.rotation_x = pos_x
-        self.rotation_y = pos_x
+        self.position_y = pos_y
+        self.rotation_x = rot_x
+        self.rotation_y = rot_y
         self.scale_x = scl_x
         self.scale_y = scl_y
+
+
+class Component:
+
+    def __init__(self):
+        pass
 
 
 def clamp(value: Union[int, float], min_value: Union[int, float], max_value: Union[int, float]):
