a61c2e10fa5b937975a9433c9e6e8d700107d2ef
Artemuy <bibikonfikus1234@gmail.com>
2022-01-27 15:39:59 +0300

    Scenes

--- Board/main.py
+++ Board/main.py
@@ -1,8 +1,4 @@
-import sys
 import pygame
-from copy import deepcopy
-import typing
-import pygame_menu
 import json
 
 
@@ -10,70 +6,56 @@ class Game:
 
     def __init__(self):
         pygame.init()
-        self.size = pygame.display.get_desktop_sizes()[0]
         self.fps = 60
         self.game_name = "Our game"
-        self.scene = 0
-        self.init_ui()
+        self.current_scene = 0
+        self.running = True
+        self.screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
+        menu = Menu(self)
+        editor = Editor(self)
+        self.scenes = [menu, editor]
         self.start()
 
-    def init_ui(self):
-        x = self.size[0]
-        y = self.size[0]
-        self.ui_controller = UIController()
-        self.button_editor = Button(self.ui_controller, "Редактор", lambda: self.set_scene(1), (x // 2 - 50,
-                                                                                                y // 2 - 10),
-                                    (x // 2 + 50, y // 2 - 60))
+    def stop(self):
+        self.running = False
 
     def set_scene(self, id):
-        self.scene = id
+        self.current_scene = id
+        self.scenes[self.current_scene].restart()
 
     def start(self):
-        screen = pygame.display.set_mode(flags=pygame.FULLSCREEN)
         pygame.display.set_caption(self.game_name)
         pygame.display.flip()
         clock = pygame.time.Clock()
-        running = True
         # menu = pygame_menu.menu.Menu(Game.GameName, width, height, pygame_menu.themes.THEME_DEFAULT)
         # menu.mainloop(screen)
-        while running:
+        while self.running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    running = False
+                    self.running = False
                 elif event.type == pygame.KEYDOWN:
                     self.get_key_down(event.key)
                 elif event.type == pygame.KEYUP:
                     self.get_key_up(event.key)
                 elif event.type == pygame.MOUSEBUTTONDOWN:
                     self.get_mouse_down(event.button, event.pos)
-            screen.fill((0, 0, 0))
+            self.screen.fill((0, 0, 0))
             self.update()
             pygame.display.flip()
             clock.tick(self.fps)
         pygame.quit()
 
     def update(self):
-        if self.scene == 0:
-            self.draw_menu()
-        elif self.scene == 1:
-            self.draw_editor()
-        elif self.scene == 2:
-            pass
+        self.scenes[self.current_scene].update()
 
-    def draw_menu(self):
-        pass
-
-    def draw_editor(self):
-        pass
-
-    def get_key_down(self, key: pygame):
-        pass
+    def get_key_down(self, key):
+        self.scenes[self.current_scene].get_key_down(key)
 
     def get_key_up(self, key):
-        pass
+        self.scenes[self.current_scene].get_key_up(key)
 
     def get_mouse_down(self, key, pos):
-        pass
+        self.scenes[self.current_scene].get_mouse_down(key, pos)
 
 
 class UIController:
@@ -81,21 +63,31 @@ class UIController:
     def __init__(self, screen):
         self.elements = []
         self.screen = screen
+        self.width = self.screen.get_width()
+        self.height = self.screen.get_height()
 
     def update(self):
         for element in self.elements:
-            element.update(self.screen)
+            element.update()
+
+    def get_click(self, pos):
+        for element in self.elements:
+            element.get_click(pos)
 
 
 class UIElement:
 
     def __init__(self, controller):
         self.controller = controller
+        self.controller.elements.append(self)
 
-    def update(self, screen):
-        self.draw(screen)
+    def update(self):
+        self.draw()
 
-    def draw(self, screen):
+    def draw(self):
+        pass
+
+    def get_click(self, pos):
         pass
 
 
@@ -109,78 +101,118 @@ class Button(UIElement):
         self.pos2 = pos2
         self.color = color
 
-    def draw(self, screen):
-        pygame.draw.rect(screen, self.color, pygame.Rect(screen.))
+    def draw(self):
+        pygame.draw.rect(self.controller.screen, self.color, pygame.Rect(self.pos1[0], self.pos1[1],
+                                                                         self.pos2[0] - self.pos1[0],
+                                                                         self.pos2[1] - self.pos1[1]))
+        font = pygame.font.SysFont("Arial", (self.pos2[1] - self.pos1[1]) // 2)
+        self.controller.screen.blit(font.render(self.text, True, pygame.Color(0, 0, 0)), self.pos1)
 
     def get_click(self, pos):
         if (self.pos1[0] <= pos[0] <= self.pos2[0]) and (self.pos1[1] <= pos[1] <= self.pos2[1]):
             self.function()
 
 
-class TestMenu:
+class List(UIElement):
 
-    def __init__(self, menu, screen):
-        self.menu = menu
-        self.menu.mainloop(screen)
-
-
-class Menu:
-
-    def __init__(self, screen: pygame.Surface, width: int, height: int):
-        self.main = pygame_menu.Menu(Game.GameName, width, height, pygame_menu.themes.THEME_BLUE)
-        self.main.mainloop(screen)
-        self.pause_menu = pygame_menu.Menu(Game.GameName, width, height, pygame_menu.themes.THEME_GREEN)
-        self.editor = pygame_menu.Menu(Game.GameName, width, height, pygame_menu.themes.THEME_SOLARIZED)
-        self.playing = pygame_menu.Menu(Game.GameName, width, height, pygame_menu.themes.THEME_DEFAULT)
-        self.menus = (self.main, self.pause_menu, self.editor, self.playing)
-        self.current_scene = self.main
-        self.screen = screen
-
-        self.init_main()
-        self.init_pause_menu()
-        self.init_editor()
-        self.init_playing()
-
-        self.switch(self.main)
-
-    def init_main(self):
-        self.main.mainloop(self.screen)
-        self.main.add.label(Game.GameName)
-
-    def init_pause_menu(self):
-        self.pause_menu.mainloop(self.screen)
-        pass
-
-    def init_editor(self):
-        self.editor.mainloop(self.screen)
-        pass
-
-    def init_playing(self):
-        self.playing.mainloop(self.screen)
-        pass
-
-    def switch(self, menu: pygame_menu.Menu):
-        for m in self.menus:
-            m.disable()
-        menu.enable()
-
-    def pause(self):
-        self.switch(self.playing)
-
-    def unpause(self):
-        self.switch(self.current_scene)
-
-    def quit(self):
-        pygame.quit()
+    def __init__(self, controller, pos1, pos2, color):
+        super(List, self).__init__(controller)
 
 
 class Level:
     pass
 
 
+class Scene:
+
+    def __init__(self, game):
+        self.game = game
+
+    def update(self):
+        pass
+
+    def get_key_down(self, key):
+        pass
+
+    def get_key_up(self, key):
+        pass
+
+    def get_mouse_down(self, key, pos):
+        pass
+
+    def restart(self):
+        pass
+
+
+class Menu(Scene):
+
+    def __init__(self, game):
+        super(Menu, self).__init__(game)
+        x = self.game.screen.get_width()
+        y = self.game.screen.get_height()
+        self.ui_controller = UIController(self.game.screen)
+        button_editor = Button(self.ui_controller, "Редактор", lambda: self.game.set_scene(1),
+                               (x // 2 - 50, y // 2 - 60),
+                               (x // 2 + 50, y // 2 - 10), pygame.Color(255, 255, 255))
+        button_quit = Button(self.ui_controller, "Выход", lambda: self.game.stop(),
+                             (x // 2 - 50, y // 2 + 10),
+                             (x // 2 + 50, y // 2 + 60), pygame.Color(255, 255, 255))
+
+    def update(self):
+        self.ui_controller.update()
+
+    def get_mouse_down(self, key, pos):
+        if key == 1:
+            self.ui_controller.get_click(pos)
+
+
+class Editor(Scene):
+
+    def __init__(self, game):
+        super(Editor, self).__init__(game)
+        self.is_playing = False
+
+        x = self.game.screen.get_width()
+        y = self.game.screen.get_height()
+        self.ui_controller = UIController(self.game.screen)
+        button_quit_editor = Button(self.ui_controller, "Назад", lambda: self.quit_editor(),
+                                    (x - 100, y - 50),
+                                    (x - 10, y - 10), pygame.Color(255, 255, 255))
+
+        button_play = Button(self.ui_controller, "Играть", lambda: self.is_playing != self.is_playing,
+                             (x - 100, 10),
+                             (x - 10, 50), pygame.Color(255, 255, 255))
+        self.board = GameField(self.game.screen, 10, 10)
+        self.board.set_view(10, 10, 50)
+        self.inventory = GameFieldInventory(self.game.screen, 10)
+        self.inventory.set_view(10, 50 * 11 + 10, 50)
+
+    def restart(self):
+        self.is_playing = False
+
+    def quit_editor(self):
+        self.is_playing = False
+        self.game.set_scene(0)
+
+    def update(self):
+        self.ui_controller.update()
+        self.board.render()
+        self.inventory.render()
+
+    def get_key_down(self, key):
+        if key == pygame.K_w:
+            pass
+
+    def get_mouse_down(self, key, pos):
+        if key == 1:
+            self.ui_controller.get_click(pos)
+            self.board.get_click(pos)
+            self.inventory.get_click(pos)
+
+
 class Board:
     # создание поля
-    def __init__(self, width, height):
+    def __init__(self, screen, width, height):
         self.width = width
         self.height = height
         self.board = [[0] * width for _ in range(height)]
@@ -188,6 +220,7 @@ class Board:
         self.left = 10
         self.top = 10
         self.cell_size = 100
+        self.screen = screen
 
     # настройка внешнего вида
     def set_view(self, left, top, cell_size):
@@ -195,12 +228,13 @@ class Board:
         self.top = top
         self.cell_size = cell_size
 
-    def render(self, screen: pygame.Surface):
-        for i in range(self.width):
-            for j in range(self.height):
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), pygame.Rect(i * self.cell_size + self.left,
-                                                                                  j * self.cell_size + self.top,
-                                                                                  self.cell_size, self.cell_size), 1)
+    def render(self):
+        for i in range(self.width + 1):
+            pygame.draw.line(self.screen, pygame.Color(255, 255, 255), (self.left + i * self.cell_size, self.top),
+                             (self.left + i * self.cell_size, self.top + self.height * self.cell_size))
+        for i in range(self.height + 1):
+            pygame.draw.line(self.screen, pygame.Color(255, 255, 255), (self.left, self.top + i * self.cell_size),
+                             (self.left + self.width * self.cell_size, self.top + i * self.cell_size))
 
     def get_cell(self, mouse_pos):
         x, y = mouse_pos
@@ -221,44 +255,47 @@ class Board:
 
 class GameField(Board):
 
-    def __init__(self, width, height):
-        super(GameField, self).__init__(width, height)
-        self.inventory = GameFieldInventory(10)
-        for i in range(width):
-            for j in range(height):
-                self.board[i][j] = GameObject(self, i, j, None)
+    def __init__(self, screen, width, height):
+        super(GameField, self).__init__(screen, width, height)
+        # for i in range(width):
+        #     for j in range(height):
+        #         self.board[i][j] = GameObject(self, i, j, None)
 
-    def render(self, screen: pygame.Surface):
-        super(GameField, self).render(screen)
-        for i in range(self.width):
-            for j in range(self.height):
-                self.board[i][j].render(screen)
-        self.inventory.render(screen)
+    def render(self):
+        super(GameField, self).render()
+
+        # super(GameField, self).render(self.screen)
+        # for i in range(self.width):
+        #     for j in range(self.height):
+        #         self.board[i][j].render(self.screen)
+        # self.inventory.render(self.screen)
 
     def on_click(self, cell_coords):
-        if type(self.board[cell_coords[0]][cell_coords[1]]) != Player:
-            self.board[cell_coords[0]][cell_coords[1]] = GameObject(self, cell_coords[0], cell_coords[1],
-                                                                    "textures/bricks.png", True)
+        pass
+        # if type(self.board[cell_coords[0]][cell_coords[1]]) != Player:
+        #     self.board[cell_coords[0]][cell_coords[1]] = GameObject(self, cell_coords[0], cell_coords[1],
+        #                                                             "textures/bricks.png", True)
 
 
 class GameFieldInventory(Board):
 
-    def __init__(self, width):
-        super(GameFieldInventory, self).__init__(width, 1)
+    def __init__(self, screen, width):
+        super(GameFieldInventory, self).__init__(screen, width, 1)
         self.active_cell = 0
 
     def on_click(self, cell_coords):
         self.active_cell = cell_coords[0]
 
-    def render(self, screen: pygame.Surface):
-        super(GameFieldInventory, self).render(screen)
-        for i in range(self.width):
-            self.board[0][0].render(screen)
+    def render(self):
+        super(GameFieldInventory, self).render()
+        # for i in range(self.width):
+        #     self.board[0][0].render(self.screen)
 
 
-class GameObject:
+class GameObject(pygame.sprite.Sprite):
 
     def __init__(self, board: Board, x: int, y: int, texture: str, collide=False):
+        super(GameObject, self).__init__(self)
         self.x = x
         self.y = y
         self.game_field = board
@@ -280,11 +317,17 @@ class GameObject:
         self.y = y
         self.game_field.board[x][y] = self
 
-    def serialize(self) -> str:
+    def get_id(self):
+        return -1
+
+    def serialize(self):
+        pass
+
+    def deserialize(self, data):
         pass
 
 
-class Player(GameObject):
+class Player(GameObject, pygame.sprite.Sprite):
 
     def __init__(self, x: int, y: int, board: GameField, texture: str):
         super(Player, self).__init__(board, x, y, texture)
@@ -316,8 +359,15 @@ class Block(GameObject):
     def __init__(self, x, y, board, texture: str):
         super(Block, self).__init__(board, x, y, texture, True)
 
-    def serialize(self) -> str:
-        json.dump()
+    def get_id(self):
+        return 0
+
+    def serialize(self):
+        return json.dumps({"id": self.get_id(), "pos": [self.x, self.y], "texture": self.texture})
+
+    def deserialize(self, data):
+        pass
+        # return Block(data["pos"][0], data["pos"][1])
 
 
 # pygame.init()
